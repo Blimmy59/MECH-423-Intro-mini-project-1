1f794be38d6cc09be38e8537ce760526998c4d0c
Blimmy59 <31900572+Blimmy59@users.noreply.github.com>
2017-09-22 13:21:51 -0700

    Final Revision

--- MECH 423 Lab Intro/Form1.Designer.cs
+++ MECH 423 Lab Intro/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MECH_423_Lab_Intro
 {
-    partial class Form1
+    partial class frmMainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,124 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label lblY;
+            System.Windows.Forms.Label lblX;
+            System.Windows.Forms.Label lblDeltaT;
+            this.txtXCoord = new System.Windows.Forms.TextBox();
+            this.txtYCoord = new System.Windows.Forms.TextBox();
+            this.lstDeltaTime = new System.Windows.Forms.ListBox();
+            this.txtDrawingWindow = new System.Windows.Forms.TextBox();
+            this.tmrMoveDelayCounter = new System.Windows.Forms.Timer(this.components);
+            lblY = new System.Windows.Forms.Label();
+            lblX = new System.Windows.Forms.Label();
+            lblDeltaT = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblY
+            // 
+            lblY.AutoSize = true;
+            lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblY.Location = new System.Drawing.Point(12, 126);
+            lblY.Name = "lblY";
+            lblY.Size = new System.Drawing.Size(63, 42);
+            lblY.TabIndex = 0;
+            lblY.Text = "Y :";
+            // 
+            // lblX
+            // 
+            lblX.AutoSize = true;
+            lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblX.Location = new System.Drawing.Point(12, 45);
+            lblX.Name = "lblX";
+            lblX.Size = new System.Drawing.Size(63, 42);
+            lblX.TabIndex = 1;
+            lblX.Text = "X :";
+            // 
+            // lblDeltaT
+            // 
+            lblDeltaT.AutoSize = true;
+            lblDeltaT.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblDeltaT.Location = new System.Drawing.Point(12, 192);
+            lblDeltaT.Name = "lblDeltaT";
+            lblDeltaT.Size = new System.Drawing.Size(138, 42);
+            lblDeltaT.TabIndex = 2;
+            lblDeltaT.Text = "Delta T";
+            // 
+            // txtXCoord
+            // 
+            this.txtXCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXCoord.Location = new System.Drawing.Point(81, 45);
+            this.txtXCoord.Name = "txtXCoord";
+            this.txtXCoord.Size = new System.Drawing.Size(134, 49);
+            this.txtXCoord.TabIndex = 3;
+            // 
+            // txtYCoord
+            // 
+            this.txtYCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYCoord.Location = new System.Drawing.Point(81, 126);
+            this.txtYCoord.Name = "txtYCoord";
+            this.txtYCoord.Size = new System.Drawing.Size(134, 49);
+            this.txtYCoord.TabIndex = 4;
+            // 
+            // lstDeltaTime
+            // 
+            this.lstDeltaTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lstDeltaTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstDeltaTime.FormattingEnabled = true;
+            this.lstDeltaTime.ItemHeight = 42;
+            this.lstDeltaTime.Location = new System.Drawing.Point(19, 237);
+            this.lstDeltaTime.Name = "lstDeltaTime";
+            this.lstDeltaTime.Size = new System.Drawing.Size(196, 592);
+            this.lstDeltaTime.TabIndex = 5;
+            // 
+            // txtDrawingWindow
+            // 
+            this.txtDrawingWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDrawingWindow.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDrawingWindow.Location = new System.Drawing.Point(262, 45);
+            this.txtDrawingWindow.Multiline = true;
+            this.txtDrawingWindow.Name = "txtDrawingWindow";
+            this.txtDrawingWindow.ReadOnly = true;
+            this.txtDrawingWindow.Size = new System.Drawing.Size(788, 836);
+            this.txtDrawingWindow.TabIndex = 6;
+            this.txtDrawingWindow.Text = "Drawing Window";
+            this.txtDrawingWindow.MouseMove += new System.Windows.Forms.MouseEventHandler(this.txtDrawingWindow_MouseMove);
+            // 
+            // tmrMoveDelayCounter
+            // 
+            this.tmrMoveDelayCounter.Enabled = true;
+            this.tmrMoveDelayCounter.Interval = 50;
+            this.tmrMoveDelayCounter.Tick += new System.EventHandler(this.tmrMoveDelayCounter_Tick);
+            // 
+            // frmMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 924);
+            this.Controls.Add(this.txtDrawingWindow);
+            this.Controls.Add(this.lstDeltaTime);
+            this.Controls.Add(this.txtYCoord);
+            this.Controls.Add(this.txtXCoord);
+            this.Controls.Add(lblDeltaT);
+            this.Controls.Add(lblX);
+            this.Controls.Add(lblY);
+            this.MinimumSize = new System.Drawing.Size(620, 420);
+            this.Name = "frmMainWindow";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtXCoord;
+        private System.Windows.Forms.TextBox txtYCoord;
+        private System.Windows.Forms.ListBox lstDeltaTime;
+        private System.Windows.Forms.TextBox txtDrawingWindow;
+        private System.Windows.Forms.Timer tmrMoveDelayCounter;
     }
 }
 
--- MECH 423 Lab Intro/Form1.cs
+++ MECH 423 Lab Intro/Form1.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace MECH_423_Lab_Intro
 {
-    public partial class Form1 : Form
+    public partial class frmMainWindow : Form
     {
-        public Form1()
+        //class Variables
+        int timeCounter;
+        int currentTime;
+        int itemLimit = 20;
+
+
+        public frmMainWindow()
         {
             InitializeComponent();
+        }
+
+        private void txtDrawingWindow_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Sets the value of fields txt X and Y to the cordinates of X and Y within the Drawing Box
+            txtXCoord.Text = e.X.ToString("0000");
+            txtYCoord.Text = e.Y.ToString("0000");
+
+            //
+            currentTime = timeCounter * tmrMoveDelayCounter.Interval;
+            lstDeltaTime.Items.Add(currentTime.ToString("000000"));
+            timeCounter = 0;
+
+            //Limits list box entries to itemLimit, and removes old entries
+            while (lstDeltaTime.Items.Count > itemLimit)
+            {
+                lstDeltaTime.Items.RemoveAt(0);
+            }
+            lstDeltaTime.SelectedIndex = lstDeltaTime.Items.Count - 1;
+        }
+
+        private void tmrMoveDelayCounter_Tick(object sender, EventArgs e)
+        {
+            timeCounter++;
         }
     }
 }
